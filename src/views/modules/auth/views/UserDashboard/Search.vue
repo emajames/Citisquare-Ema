518d30fb60891ff1cad27363e4951a7ccd8379ea
emajames <emaediongjames508@gmail.com>
2022-05-12 18:39:14 +0100

    newpage

--- src/views/modules/auth/views/UserDashboard/Search.vue
+++ src/views/modules/auth/views/UserDashboard/Search.vue
@@ -5,16 +5,48 @@
     </div>
     <Sidebar/>
     <div class="dash1">
-        <div class="nav_cont d-flex">
-            <h2>Search</h2>
-            <div class="btn_items">
-                <button class="inactive">Inactive</button>
-                 <!-- <FontAwesomeIcon :icon="['far', 'bell']" class="icon" /> -->
-            </div>
+        <div class="contanier">
+             <div class="nav_cont">
+            <h2 class="search">Search</h2>
+
+             <div class="box">
+          <div class="" >
+              <p>Location</p>
+              <div class="location">
+                  <FontAwesomeIcon :icon="['fas','location-dot']" class="icon" />
+                  <input type="text" placeholder="Lekki,Lagos">
+              </div>  
+          </div>
+
+         <div class="" >
+              <p>Category</p>
+              <div class="location">
+                  <FontAwesomeIcon :icon="['fas', 'chevron-down']" class="icons" />
+                  <input type="text" placeholder="Lekki,Lagos">
+              </div>  
+          </div>
+
+           <div class="" >
+              <p>Sub Category</p>
+              <div class="location">
+                  <FontAwesomeIcon :icon="['fas', 'chevron-down']" class="icons" />
+                  <input type="text" placeholder="Lekki,Lagos">
+              </div>  
+          </div>
+              <div>
+                  <button>Search</button>
+              </div>        
+      </div>
+      <div class="pt-3">
+          <h3 class="fin">30 Financial providers that match your search</h3>
+      </div>
+
         </div>
-        <div class="dash2">
             
         </div>
+       
+        
+        <div class="dash2"></div>
     </div>
 </div>
     
@@ -51,11 +83,77 @@
         width: 80%;
     }
     .dash2{
-        background: #FAFBFA;
+        /* background: #FAFBFA; */
         height: 100vh;
         width: 100%;
         margin-top: 1rem;
     }
+    .search {
+    padding-top: 1rem;
+    font-weight: 700;
+    font-size: 30px;
+    letter-spacing: -0.01em;
+    color: #08293A;
+}
+
+.box {
+    /* border: 0.5px #e0e8ec solid; */
+    box-shadow: 0 3px 10px rgb(0 0 0 / 0.1);;
+    height: 135px;
+    padding: 15px 15px;
+    display: grid;
+    grid-template-columns: 2fr 1fr 1fr 1fr;
+    column-gap: 1.5rem;
+    margin-top: 3rem;
+    border-radius: 3px;
+}
+.box p{
+    font-size: 12px;
+    font-weight: 400;
+    color:  #08293A;
+}
+.location {
+    display: flex;
+    position: relative;
+    
+}
+
+.location input{
+    padding: 3.7px 15px;
+    border: 0.5px #e0e8ec solid;
+    border-radius: 3px;
+    width: 100%;
+    height: 45px;
+}
+.icon {
+   position: absolute;
+   left: 21rem;
+   top: 0.8rem;
+   color: #cad4dd;
+}
+.icons {
+   position: absolute;
+   left: 9.5rem;
+   top: 0.8rem;
+   color: #cad4dd;
+}
+button {
+    width: 100%;
+    margin-top: 1.5rem;
+    padding: 4px 10px;
+    outline: none;
+    border: none;
+    border-radius: 3px;
+    height: 45px;
+    background: rgba(22, 134, 193, 1);
+    color: #fff;
+}
+.fin {
+    font-size: 16px;
+    font-weight: 600;
+    letter-spacing: -0.01em;
+    color: #08293A;
+}
     
 
 
